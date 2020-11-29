f3a9f9c51f8fa2dd4bf82f87bd36916e3671ce0f
MuhammadSiddiq <707myworld@gmail.com>
2020-11-29 22:35:15 +0500

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var elTodoInput = $_('.todo-input', elTodoForm);
 var elTodoList = $_('.todo-list', elTodoForm);
 
 var elTodoTemplate = $_('#todo-item').content;
-var todoCheckbox = $_('.todo-checkbox', elTodoTemplate);
+var elTodoCheckbox= $_('.todo-checkbox', elTodoTemplate);
 
 var elClearBtn = $_('.clear-btn');
 var elShowAllBtn = $_('.show-all');
@@ -24,51 +24,58 @@ var createNewTodo = function () {
     $_('.todo-text', newTodo).textContent = todo.title;
     $_('.todo-checkbox', newTodo).dataset.id = todo.id;
     $_('.todo-text', newTodo).dataset.id = todo.id;
+
     newTodoFragment.appendChild(newTodo);
   });
 
   elTodoList.appendChild(newTodoFragment);
 }
-createNewTodo();
 
-todoCheckbox.checked
+createNewTodo();
 
 
 
 elTodoForm.addEventListener('submit', evt => {
   evt.preventDefault();
-  elTodoList.innerHTML = '';  
+  elTodoList.innerHTML = '';
 
   var enteredValue = elTodoInput.value.trim();
-
   var searchWord = new RegExp(enteredValue, "gi")
   var isExist;
+
   todosArray.forEach(function(todo) {
     isExist = todo.title.match(searchWord);
-  })
+  });
 
   var i = todosArray.length;
   if (!isExist) {
     todosArray.push({
       title: enteredValue,
       id: i+1,
-      isUrgent: false,
       complated: false
     })
+
     localStorage.setItem('todos', JSON.stringify(todosArray));
-  }
-  console.log(todosArray);
+  };
 
   createNewTodo();
   elTodoInput.value = '';
 });
+
+elTodoList.addEventListener('click', function(evt){
+  evt.target.matches('.todo-checkbox');
+
+  if (evt.target.matches('.todo-checkbox').checked) {
+    console.log('Bosvoldiz')
+  } else {
+    console.log('Bosmadizku kuch bormi ozi')
+  }
+
+});
+
 
 elClearBtn.addEventListener('click', function(evt){
   elTodoList.innerHTML = '';
   todosArray = [];
   localStorage.clear();
 });
-
-elShowAllBtn.addEventListener("click", function(evt) {
-
-})
